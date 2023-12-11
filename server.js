f39534f2295360d8bed526c1eb17259ba1141db7
14089 <baoxiong814@gmail.com>
2023-12-10 20:39:49 -0600

    added starting folder and files

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+//const helpers = require('./utils/');
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new sequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+//const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
