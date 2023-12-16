a7b71298b15d582ecb778fcc1e1addab0e4e6401
bxiong34 <111828461+bxiong34@users.noreply.github.com>
2023-12-15 23:39:09 -0600

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,21 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_STRING);
 
+if (process.env.DB_STRING) {
+    sequelize = new Sequelize(process.env.DB_STRING);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+      }
+    );
+  }
+
 //   } else {
 //     sequelize = new Sequelize(
 //       process.env.DB_NAME,
--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
 const sequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
